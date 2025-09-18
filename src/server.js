ee44f8e8d1c6ea9b7e0bf3dfa880786a6dbf7435
Shakib Hossain <shakibhossain2020124@gmail.com>
2025-09-18 06:17:26 +0600

    First backend program setup

---
+++ src/server.js
@@ -0,0 +1,8 @@
+import http from 'http'
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{"content-type":"text/html"})
+    res.end("<h1>Hello WOrld!</h1>")
+})
+
+server.listen(8000, ()=> console.log("Server is running on port 8000"))
