ef80f4536ac8ef188a947d6f0ef8ba1a8e66c1f1
Shakib Hossain <shakibhossain2020124@gmail.com>
2025-09-18 06:40:05 +0600

    Uses fs and path to read and publich index.html

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 import http from 'http'
+import fs from 'fs'
+import path from 'path'
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{"content-type":"text/html"})
-    res.end("<h1>Hello WOrld!</h1>")
+    const filePath = path.join(process.cwd(),"src/index.html")
+    fs.readFile(filePath, (err,data)=>{
+        if(err){
+            res.writeHead(500,{"content-type":"text/plain"})
+            res.end("error loading index.html")
+        }
+        else{
+            res.writeHead(200,{"content-type":"text/html"})
+            res.end(data)
+        }
+
+    })
+    
 })
 
 server.listen(8000, ()=> console.log("Server is running on port 8000"))
